39176412f5586d22671ba7c4c416550f3b15f5a2
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-03-12 19:15:10 -0300

    Adicionando Services para requisições pelo Client

---
+++ BlazorShop.Web/Services/ProdutoService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorShop.Models.DTOs;
+using System.Net.Http.Json;
+
+namespace BlazorShop.Web.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+        public ProdutoService(HttpClient httpClient, ILogger<ProdutoService> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+ 
+       
+        public HttpClient _httpClient;
+        public ILogger<ProdutoService> _logger;
+
+
+        public async Task<IEnumerable<ProdutoDTO>> GetItens()
+        {
+            try
+            {
+                var produtoDto = await _httpClient.GetFromJsonAsync<IEnumerable<ProdutoDTO>>("api/produtos");
+                return produtoDto;
+
+            }
+            catch (Exception)
+            {
+
+                _logger.LogError("Erro ao acessar produtos : api/produtos");
+            }
+        }
+    }
+}
